961879140d72b819ded3b1ee5a319dffa47c15a9
Triin Vaht <triinvaht@gmail.com>
2019-05-24 15:04:16 +0300

    Complete our Implementation

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,3 +1,16 @@
-function AddressBook() {
 
+//Aadressiraamat
+function AddressBook() {
+    //Kontaktide list
+    this.contacts = [];
 }
+
+//addContact lisab kontakti kontaktide listi
+AddressBook.prototype.addContact = function (contact) {
+    this.contacts.push(contact);
+};
+
+//getContact leiab id'ga kontakti listist
+AddressBook.prototype.getContact = function (index) {
+    return this.contacts[index];
+};
